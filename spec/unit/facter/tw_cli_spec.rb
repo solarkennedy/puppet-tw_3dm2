f29ee1fa4eb0f075d194532cf9d6b9b159fdb08c
kwa <kwa@yelp.com>
2013-12-03 15:01:37 -0800

    Added more broken tests

--- spec/unit/facter/tw_cli_spec.rb
+++ spec/unit/facter/tw_cli_spec.rb
@@ -2,31 +2,22 @@ require 'spec_helper'
 
 describe 'tw_cli fact', :type => :fact  do
 
-  describe 'on linux' do
-    it "should find the tw_cli binary" do
-      Facter.fact(:kernel).stubs(:value).returns('Linux')
-      Facter::Util::Resolution.stubs(:which).with('tw_cli').returns('/usr/sbin/tw_cli')
-
-      Facter.fact(:tw_cli).value.should == '/usr/sbin/tw_cli'
-    end
-
-    it "should not find the tw_cli binary" do
-      Facter.fact(:kernel).stubs(:value).returns('Linux')
-      Facter::Util::Resolution.stubs(:which).with('tw_cli').returns(nil)
-
-      Facter.value('tw_cli').should == nil
-    end
+  it "should find the tw_cli binary on linux when it exists" do
+    Facter.fact(:kernel).stubs(:value).returns('Linux')
+    Facter::Util::Resolution.stubs(:which).with('tw_cli').returns('/usr/sbin/tw_cli')
+    Facter::Util::Resolution.stubs(:which).with('/usr/sbin/tw_cli').returns('/usr/sbin/tw_cli')
+    Facter.fact(:tw_cli).value.should == '/usr/sbin/tw_cli'
   end
-  
-  describe 'on no kernel' do
-    it "should not find the tw_cli binary" do
-      Facter::Util::Resolution.stubs(:which).with('tw_cli').returns('/usr/sbin/tw_cli')
 
-      Facter.value('tw_cli').should == nil
-    end
-  
-    it "should not find the tw_cli binary" do
-      Facter.value('tw_cli').should == nil
-    end
+  it "should not find the tw_cli binary on linux when it doesnt exist" do
+    Facter.fact(:kernel).stubs(:value).returns('Linux')
+    Facter::Util::Resolution.stubs(:which).with('tw_cli').returns(nil)
+    Facter.value('tw_cli').should == nil
   end
+
+  it "When not Linux, the fact should not exist" do
+    Facter.fact(:kernel).stubs(:value).returns('Solaris')
+    Facter.value('tw_cli').should == nil
+  end
+
 end
--- spec/unit/facter/tw_controllers.rb
+++ spec/unit/facter/tw_controllers.rb
@@ -1,13 +1,9 @@
 require 'spec_helper'
 
-RSpec.configure do |config|
-  config.mock_framework = :mocha
-end
-
 describe 'tw_controllers fact' do
-  describe 'tw_cli fact set' do
+
+  it "should be able to read a single controller" do
     Facter.fact(:tw_cli).stubs(:value).returns '/usr/sbin/tw_cli'
-    Facter.fact(:tw_cli).value.should == '/usr/sbin/tw_cli'
     Facter::Util::Resolution.stubs(:exec).with('/usr/sbin/tw_cli show')
 .returns(<<EOS)
 
@@ -19,7 +15,93 @@ EOS
     Facter.fact(:tw_controllers).value.should == 'c8'
   end
 
+  it "should be able to interpret controllers with enclosures" do
+    Facter::Util::Resolution.stubs(:exec).with('/usr/sbin/tw_cli show').returns(<<EOS)
+
+Ctl   Model        (V)Ports  Drives   Units   NotOpt  RRate   VRate  BBU
+------------------------------------------------------------------------
+c6    9750-8i      12        12       1       0       1       1      OK       
+
+Enclosure     Slots  Drives  Fans  TSUnits  PSUnits  Alarms   
+--------------------------------------------------------------
+/c6/e0        12     12      3     1        2        1        
+
+EOS
+    Facter.fact(:tw_controllers).value.should == 'c6'
+  end
+
+# example output 1:
+#
+# # tw_cli show version
+# CLI Version = 2.00.11.020
+# API Version = 2.08.00.023
+#
+# # tw_cli show
+#
+# Ctl   Model        (V)Ports  Drives   Units   NotOpt  RRate   VRate  BBU
+# ------------------------------------------------------------------------
+# c8    9690SA-8I    8         8        1       0       1       1      OK       
+#
+
+# example output 2:
+#
+# # tw_cli show version
+# CLI Version = 2.00.11.016
+# API Version = 2.08.00.017
+# 
+# # tw_cli show
+# 
+# Ctl   Model        (V)Ports  Drives   Units   NotOpt  RRate   VRate  BBU
+# ------------------------------------------------------------------------
+# c0    9750-8i      7         7        3       0       1       1      OK       
+# 
+
+# example output 3:
+#
+# # tw_cli show version
+# CLI Version = 2.00.11.016
+# API Version = 2.08.00.017
+# 
+# # tw_cli show
+# 
+# Ctl   Model        (V)Ports  Drives   Units   NotOpt  RRate   VRate  BBU
+# ------------------------------------------------------------------------
+# c0    9650SE-8LPML 8         8        3       0       1       1      OK       
+# 
+
+# example output 4:
+#
+# # tw_cli show version
+# CLI Version = 2.00.06.007
+# API Version = 2.03.00.006
+# CLI Compatible Range = [2.00.00.001 to 2.00.06.007]
+#
+# # tw_cli show
+#
+# Ctl   Model        Ports   Drives   Units   NotOpt   RRate   VRate   BBU
+# ------------------------------------------------------------------------
+# c2    7506-8       8       8        2       0        3       -       -        
+# c3    7506-8       8       8        2       0        3       -       -        
+#
+
+# example of no controllers being present
+#
+# # tw_cli show version
+# CLI Version = 2.00.11.016
+# API Version = 2.08.00.017
+# 
+# # tw_cli show
+# 
+# No controller found.
+# Make sure appropriate LSI/3ware device driver(s) are loaded.
+# 
+# # echo $?
+# 0
+
+
   describe 'tw_cli fact missing' do
+    Facter.fact(:tw_cli).value.should == nil
     Facter.fact(:tw_controllers).value.should == nil
   end
+
 end
